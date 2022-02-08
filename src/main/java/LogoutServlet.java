6a0854c86e051e15a0a6ce244f2fd72352bcba73
Charles Aggasid <charles.aggasid01@gmail.com>
2022-02-08 10:57:07 -0600

    session and cookies exercise

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -5,13 +5,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(request.getSession().getAttribute("user") != null) {
-        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
-        } else {
-            response.sendRedirect("/login");
-        }
+        request.getSession().invalidate(); // will reset or invalidate the current session
+        response.sendRedirect("/login");
     }
 }

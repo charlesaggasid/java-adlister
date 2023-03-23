194545e9845f8fa90c1fc37a51c340e1c0716914
Charles Aggasid <charles.aggasid01@gmail.com>
2023-03-22 21:31:30 -0500

    added bonues

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -9,15 +9,30 @@ import java.io.PrintWriter;
 public class CounterServlet extends HttpServlet {
     private int counter = 0;
 
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//        if (request.getParameter("reset") != null){
+//            counter = 0;
+//            out.println("<p>The counter says you've never viewed this page, but you aren't fooling me</p>");
+//        }
+//        counter++;
+//        out.println("<h3>You have viewed this page " + counter + " times, aren't you bored?");
+//    }
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        if (request.getParameter("reset") != null){
+        String resetParam = request.getParameter("reset");
+        if (resetParam != null && resetParam.equals("true")){
             counter = 0;
-            out.println("<p>The counter says you've never viewed this page, but you aren't fooling me</p>");
+            out.println("<p>The counter has been reset.</p>");
         }
         counter++;
-        out.println("<h3>You have viewed this page " + counter + " times, aren't you bored?");
+        out.println("<h3>You have viewed this page " + counter + " times.</h3>");
+        out.println("<p>To reset the counter, add ?reset=true to the URL.</p>");
     }
+    
 }
